e4907e55b19ef6a9afa2924ac0de7c52d80c2c34
jjpae948 <parvathiyalapalli1105@gmail.com>
2026-07-27 15:21:48 +0530

    hello world script created

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+
+echo "Russia: Hey Ukraine,we are going blast you"
+echo "Iran: Yes,please. you are welcome"
+echo "Trump: Hey Iran Iam serious"
+echo "Iran: Hey Trump, we are also serious"
