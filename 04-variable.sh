b7a0a40d290f43d457610e60c6c3c3b3e27a3c40
jjpae948 <parvathiyalapalli1105@gmail.com>
2026-07-27 16:03:11 +0530

    hello world script created

---
+++ 04-variable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash/ 
+
+echo "please enter your username"
+read USER_NAME # USER_ NAME is variable whatever you enterin terminal stores in this variable
+echo  "Username is $USER_NAME" # $USER_NAME":
