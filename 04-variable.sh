048c36adc9a9d15d13c10330d6ef5eaf5f6d2a63
jjpae948 <parvathiyalapalli1105@gmail.com>
2026-07-27 16:09:27 +0530

    hello world script created

--- 04-variable.sh
+++ 04-variable.sh
@@ -3,3 +3,8 @@
 echo "please enter your username"
 read USER_NAME # USER_ NAME is variable whatever you enterin terminal stores in this variable
 echo  "Username is $USER_NAME" # $USER_NAME":
+
+echo "please enter your password::"
+read -s PASSWORD 
+
+echo "Password is: $PASSWORD" 
