c92286dd980635b5867ff89e7f01082e2a65d7d5
jjpae948 <parvathiyalapalli1105@gmail.com>
2026-07-28 15:53:42 +0530

    api doc added

--- 10-logs.sh
+++ 10-logs.sh
@@ -3,25 +3,31 @@
 USERID=$(id -u)
 LOGS_DIR=/home/ec2-user/shell-logs
 LOGS_FILE="$LOGS_DIR/$0.log" # home/ec2-user/shell-logs/10-logs.sh.log
+
 # Check root access or not
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
     exit 1
 fi
 
-# echo "Iam continuing..."
-dnf installed nginx
+# first arg -> what are u trying to install
+# second arg -> exit cde
+VALIDATE()
+    if [ $2 -ne 0 ]; then
+       echo "Installing $1 is ... FAILED" |tee -a LOGS_FILE
+       exit 1
+    else
+        echo "Instaling $1 is ...SUCCESS"
+    fi
 
-if [ $? -eq 0 ]; then
-    echo "nginx is already installed...SKIPPING"
-else
-  echo "Installing nginx -y&>>"
-  dnf install nginx -y&>>$LOGS_FILE
 
-  if [ $? -ne 0 ]; then
-    echo " Installing nginx &>>$LOGS_FILE"
-    exit 1
+  #echo "I am continuing..."       
+  dnf  list installed mysql &>>$LOGS_FILE
+
+  if [ $? -eq 0 ]; then
+     echo " MySQL is alredy installed ... SKIPPING" 
   else
-    echo " Installing nginx -y&>>$LOGS_FILE"
-   fi
+      echo"Installing MySQL"
+      echo " Installing mysql -y &>> $LOGS_FILE"
+   VALIDATE MySQL $?
 fi
